19edbe38c6fb3e5f0936757f73902acffd612ce2
possior <153281819+possior@users.noreply.github.com>
2026-05-02 01:29:10 +0900

    20260502 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,10 @@
 [[ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]] && ROOT="$(git rev-parse --show-toplevel)" || exit
-[[ -f "$ROOT/source/utility.sh" ]] && source "$ROOT/source/utility.sh" || exit
+[[ -f "$ROOT/source/.config/.utility.sh" ]] && source "$ROOT/source/.config/.utility.sh" || exit
 [[ -d "$HOME/.config/bash" ]] || mkdir -p "$HOME/.config/bash"
 [[ -d "$HOME/.image" ]] || mkdir -p "$HOME/.image"
 insy "$ROOT/source/.bashrc" "$HOME/.bashrc"
 insy "$ROOT/source/.bash_profile" "$HOME/.bash_profile"
+insy "$ROOT/source/.config/.utility.sh" "$HOME/.config/.utility.sh"
 insy "$ROOT/source/.config/bash/bashrc.sh" "$HOME/.config/bash/bashrc.sh"
 insy "$ROOT/source/.config/bash/alias.sh" "$HOME/.config/bash/alias.sh"
 insy "$ROOT/source/.config/bash/appimage.sh" "$HOME/.config/bash/appimage.sh"
