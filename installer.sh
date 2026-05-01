bdde7204c36e5185397beb68a1cbc9550fc105fc
possior <153281819+possior@users.noreply.github.com>
2026-05-02 01:13:56 +0900

    20260502 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -1,44 +1,14 @@
-[[ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]] && ROOT="$(git rev-parse --show-toplevel)" || return
+[[ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]] && ROOT="$(git rev-parse --show-toplevel)" || exit
+[[ -f "$ROOT/utility.sh" ]] && source "$ROOT/utility.sh" || exit
 [[ -d "$HOME/.config/bash" ]] || mkdir -p "$HOME/.config/bash"
 [[ -d "$HOME/.image" ]] || mkdir -p "$HOME/.image"
-installer() {
-  fr="$1"
-  to="$2"
-
-  if
-    [[ -f "$to" ]]
-  then
-    read -p ":: override $to? [Y/n] " ds
-  fi
-
-  ds="${ds:-Y}"
-
-  if
-    [[ "$ds" == "Y" ]] || [[ "$ds" == "y" ]]
-  then
-    cp -f "$fr" "$to"
-  elif
-    [[ "$ds" != "N" ]] && [[ "$ds" != "n" ]]
-  then
-    exit 
-  fi
-}
-
 installer "$ROOT/source/.bashrc" "$HOME/.bashrc"
 installer "$ROOT/source/.bash_profile" "$HOME/.bash_profile"
-
 installer "$ROOT/source/.config/bash/bashrc.sh" "$HOME/.config/bash/bashrc.sh"
 installer "$ROOT/source/.config/bash/alias.sh" "$HOME/.config/bash/alias.sh"
 installer "$ROOT/source/.config/bash/appimage.sh" "$HOME/.config/bash/appimage.sh"
 installer "$ROOT/source/.config/bash/promptstatement.sh" "$HOME/.config/bash/promptstatement.sh"
 installer "$ROOT/source/.config/bash/variable.sh" "$HOME/.config/bash/variable.sh"
-
 installer "$ROOT/source/.image/installer.sh" "$HOME/.image/installer.sh"
-if
-  [[ ! -f "$HOME/.image/locators.csv" ]]
-then
-  installer "$ROOT/source/.image/locators.csv" "$HOME/.image/locators.csv"
-fi
-
-source "$HOME/.bashrc"
+installer "$ROOT/source/.image/locators.csv" "$HOME/.image/locators.csv"
 
