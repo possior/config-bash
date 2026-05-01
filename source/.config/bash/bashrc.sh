10302045d0c7da6146cdf7df4f81c3bdd914e9a5
possior <153281819+possior@users.noreply.github.com>
2026-05-02 00:20:32 +0900

    20260502 | Modified /source/.config/bash/bashrc.sh

--- source/.config/bash/bashrc.sh
+++ source/.config/bash/bashrc.sh
@@ -1,26 +1,5 @@
-config="$HOME/.config/bash"
-
-if
-  [[ -f "$config/alias.sh" ]]
-then
-  source "$config/alias.sh"
-fi
-
-if
-  [[ -f "$config/appimage.sh" ]]
-then
-  source "$config/appimage.sh"
-fi
-
-if
-  [[ -f "$config/promptstatement.sh" ]]
-then
-  source "$config/promptstatement.sh"
-fi
-
-if
-  [[ -f "$config/variable.sh" ]]
-then
-  source "$config/variable.sh"
-fi
+[[ -f "$HOME/.config/bash/alias.sh" ]] && source "$HOME/.config/bash/alias.sh"
+[[ -f "$HOME/.config/bash/appimage.sh" ]] && source "$HOME/.config/bash/appimage.sh"
+[[ -f "$HOME/.config/bash/promptstatement.sh" ]] && source "$HOME/.config/bash/promptstatement.sh"
+[[ -f "$HOME/.config/bash/variable.sh" ]] && source "$HOME/.config/bash/variable.sh"
 
