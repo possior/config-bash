0c38a9d7dbca87d23b01d45fcba835a1e7c0afa0
possior <153281819+possior@users.noreply.github.com>
2026-05-02 01:23:25 +0900

    20260502 | Modified /installer.sh & Modified /source/utility.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
 [[ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]] && ROOT="$(git rev-parse --show-toplevel)" || exit
-[[ -f "$ROOT/utility.sh" ]] && source "$ROOT/utility.sh" || exit
+[[ -f "$ROOT/source/utility.sh" ]] && source "$ROOT/source/utility.sh" || exit
 [[ -d "$HOME/.config/bash" ]] || mkdir -p "$HOME/.config/bash"
 [[ -d "$HOME/.image" ]] || mkdir -p "$HOME/.image"
-installer "$ROOT/source/.bashrc" "$HOME/.bashrc"
-installer "$ROOT/source/.bash_profile" "$HOME/.bash_profile"
-installer "$ROOT/source/.config/bash/bashrc.sh" "$HOME/.config/bash/bashrc.sh"
-installer "$ROOT/source/.config/bash/alias.sh" "$HOME/.config/bash/alias.sh"
-installer "$ROOT/source/.config/bash/appimage.sh" "$HOME/.config/bash/appimage.sh"
-installer "$ROOT/source/.config/bash/promptstatement.sh" "$HOME/.config/bash/promptstatement.sh"
-installer "$ROOT/source/.config/bash/variable.sh" "$HOME/.config/bash/variable.sh"
-installer "$ROOT/source/.image/installer.sh" "$HOME/.image/installer.sh"
-installer "$ROOT/source/.image/locators.csv" "$HOME/.image/locators.csv"
+insy "$ROOT/source/.bashrc" "$HOME/.bashrc"
+insy "$ROOT/source/.bash_profile" "$HOME/.bash_profile"
+insy "$ROOT/source/.config/bash/bashrc.sh" "$HOME/.config/bash/bashrc.sh"
+insy "$ROOT/source/.config/bash/alias.sh" "$HOME/.config/bash/alias.sh"
+insy "$ROOT/source/.config/bash/appimage.sh" "$HOME/.config/bash/appimage.sh"
+insy "$ROOT/source/.config/bash/promptstatement.sh" "$HOME/.config/bash/promptstatement.sh"
+insy "$ROOT/source/.config/bash/variable.sh" "$HOME/.config/bash/variable.sh"
+insy "$ROOT/source/.image/installer.sh" "$HOME/.image/installer.sh"
+insn "$ROOT/source/.image/locators.csv" "$HOME/.image/locators.csv"
 
