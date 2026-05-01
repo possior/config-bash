430f37ca15b0035038b628c702d62403cebd62d8
possior <153281819+possior@users.noreply.github.com>
2026-05-02 00:51:53 +0900

    20260502 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 [[ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]] && ROOT="$(git rev-parse --show-toplevel)" || return
-
+[[ -d "$HOME/.config/bash" ]] || mkdir -p "$HOME/.config/bash"
+[[ -d "$HOME/.image" ]] || mkdir -p "$HOME/.image"
 installer() {
   fr="$1"
   to="$2"
@@ -26,23 +27,11 @@ installer() {
 installer "$ROOT/source/.bashrc" "$HOME/.bashrc"
 installer "$ROOT/source/.bash_profile" "$HOME/.bash_profile"
 
-if
-  [[ ! -d "$HOME/.config/bash" ]]
-then
-  mkdir -p "$HOME/.config/bash"
-fi
-
 installer "$ROOT/source/.config/bash/bashrc.sh" "$HOME/.config/bash/bashrc.sh"
 installer "$ROOT/source/.config/bash/alias.sh" "$HOME/.config/bash/alias.sh"
 installer "$ROOT/source/.config/bash/appimage.sh" "$HOME/.config/bash/appimage.sh"
 installer "$ROOT/source/.config/bash/promptstatement.sh" "$HOME/.config/bash/promptstatement.sh"
 installer "$ROOT/source/.config/bash/variable.sh" "$HOME/.config/bash/variable.sh"
-
-if
-  [[ ! -d "$HOME/.config/image" ]]
-then
-  mkdir -p "$HOME/.config/image"
-fi
 
 installer "$ROOT/source/.image/installer.sh" "$HOME/.image/installer.sh"
 if
