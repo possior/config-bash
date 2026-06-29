5c9dfb7dad7519e0e1d8685e80d3ca4ca25bc273
possior <153281819+possior@users.noreply.github.com>
2026-06-30 00:49:01 +0900

    20260630 | Added /src/bash.sh

---
+++ src/bash.sh
@@ -0,0 +1,14 @@
+PS0=""
+PS1="\n[\u@\h] \w\n$ "
+PS2="> "
+PS3=": "
+PS4="+ "
+
+export HISTFILE=${HOME}/.config/bash/history.sh
+export HISTFILESIZE=12
+export HISTSIZE=12
+
+alias grep='grep --color auto'
+alias ls='ls -A --color=auto'
+alias yay='yay --builddir /var/tmp'
+alias yay!='yay --sudoloop --builddir /var/tmp --overwrite *'
