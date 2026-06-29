7d4850839e185934ed7c243a057762d91a5eaad4
possior <153281819+possior@users.noreply.github.com>
2026-06-30 00:48:06 +0900

    20260630 | Deleted /source/.config/bash/bashrc.sh

--- source/.config/bash/bashrc.sh
+++
@@ -1,6 +0,0 @@
-[[ $- != *i* ]] && return
-[[ -f "$HOME/.config/bash/alias.sh" ]] && source "$HOME/.config/bash/alias.sh"
-[[ -f "$HOME/.config/bash/appimage.sh" ]] && source "$HOME/.config/bash/appimage.sh"
-[[ -f "$HOME/.config/bash/prompt.sh" ]] && source "$HOME/.config/bash/prompt.sh"
-[[ -f "$HOME/.config/bash/variable.sh" ]] && source "$HOME/.config/bash/variable.sh"
-
