bffeb682ebee8edd190c6ce7306e6bb25bf5b2dd
possior <153281819+possior@users.noreply.github.com>
2026-07-21 13:30:59 +0900

    20260721 | Modified /src/bash.sh

--- src/bash.sh
+++ src/bash.sh
@@ -9,7 +9,7 @@ export HISTFILESIZE="12"
 export HISTSIZE="12"
 export JULIA_EDITOR="vim"
 
-alias 'grep'='grep --color=auto'
-alias 'ls'='ls -A --color=auto'
-alias 'yay'='yay --builddir=/var/tmp --color=auto'
-alias 'yay!'='yay --sudoloop --overwrite="*"'
+alias "grep"="grep --color=auto"
+alias "ls"="ls -A --color=auto"
+alias "yay"="yay --builddir=/var/tmp --color=auto"
+alias "yay!"="yay --sudoloop --overwrite='*'"
